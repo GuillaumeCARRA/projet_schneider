ad5fca58fbdfb80ffeb3ab0c72f7db921f72f417
GuillaumeCARRA <carraguillaume@gmail.com>
2023-04-26 13:43:40 +0200

    add a file for sequelize models

--- Back/app/app.js
+++ Back/app/app.js
@@ -12,7 +12,6 @@ app.get('/', (request, response) => {
     response.send("Hello World");
 });
 
-
 const PORT = process.env.PORT || 4000; 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
