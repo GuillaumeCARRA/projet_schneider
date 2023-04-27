10e93594e3e163259d5d40ce9d26e365a4fc8785
GuillaumeCARRA <carraguillaume@gmail.com>
2023-04-27 10:37:44 +0200

    started to configure sequelize models

--- Back/app/models/product.js
+++ Back/app/models/product.js
@@ -3,3 +3,20 @@ import sequelize from "../db/client.js";
 import {DataTypes, Model} from "sequelize";
 
 class Product extends Model{};
+
+Product.init(
+    {
+        product_name: DataTypes.STRING,
+        product_img: DataTypes.BLOB, 
+        product_description: DataTypes.STRING,
+        product_price: DataTypes.INTEGER
+    },
+
+    {
+        sequelize, 
+
+        tableName:"product"
+    }
+);
+
+export default Product; 
--- Back/app/models/role.js
+++ Back/app/models/role.js
@@ -3,3 +3,17 @@ import sequelize from "../db/client.js";
 import {DataTypes, Model} from "sequelize";
 
 class Role extends Model{};
+
+Role.init(
+    {
+        label: DataTypes.STRING
+    },
+
+    {
+        sequelize, 
+
+        tableName:"role"
+    }
+);
+
+export default Role; 
