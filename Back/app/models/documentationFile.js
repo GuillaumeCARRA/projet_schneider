5275ed8db948786b98f7d47a1bfd98a99b136f8e
GuillaumeCARRA <carraguillaume@gmail.com>
2023-04-28 10:52:37 +0200

    add comments

--- Back/app/models/documentationFile.js
+++ Back/app/models/documentationFile.js
@@ -2,10 +2,14 @@ import sequelize from "../db/client.js";
 
 import {DataTypes, Model} from "sequelize";
 
+// Définition d'une classe DocumentationCategory 
+// qui hérite de la classe Model de Sequelize
 class DocumentationFile extends Model{};
 
 DocumentationFile.init(
     {
+        // Définition du modèle de la table documentation_category, 
+        // avec un champ documentation_category_name de type STRING
         documentation_file_name: DataTypes.STRING,
         documentation_file_format: DataTypes.STRING,
         documentation_file_img: DataTypes.BLOB, 
@@ -14,8 +18,11 @@ DocumentationFile.init(
     },
 
     {
+        // Spécification de l'instance Sequelize à utiliser 
+        // pour se connecter à la base de données
         sequelize, 
 
+        // Spécification du nom de la table dans la base de données correspondant à ce modèle
         tableName:"documentation_file"
     }
 );
