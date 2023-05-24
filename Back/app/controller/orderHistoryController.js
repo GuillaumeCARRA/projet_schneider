cadcec358e258071d149fd8131a8f85c6b05c374
GuillaumeCARRA <carraguillaume@gmail.com>
2023-05-24 11:46:30 +0200

    ending controllers

--- Back/app/controller/orderHistoryController.js
+++ Back/app/controller/orderHistoryController.js
@@ -1,1 +1,40 @@
 import OrderHistory from "../models/orderHistory.js";
+
+const getAllOrderHistories = async (req, res) => {
+    try {
+        const orderHistories = await OrderHistory.findAll({
+            include: [
+                {association: "orders"}
+            ]
+        });
+
+        res.json({data: orderHistories}); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const getOneOrderHistory = async (req, res) => {
+    try {
+        const orderHistoryId = req.params.id; 
+
+        const oneOrderHistory = await OrderHistory.findOne({
+            where: ({id: orderHistoryId}), 
+            include: [
+                {association: "orders"}
+            ]
+        });
+
+        res.json({data: oneOrderHistory}); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+export default {
+    getAllOrderHistories, 
+    getOneOrderHistory
+}
--- Back/app/controller/productController.js
+++ Back/app/controller/productController.js
@@ -1,1 +1,127 @@
 import Product from "../models/product.js";
+
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.findAll({
+            include: [
+                {association: "orders"}
+            ]
+        });
+
+        res.json({data: products}); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const getOneProduct = async (req, res) => {
+    try {
+        const productId = req.params.id; 
+
+        const oneProduct = await Product.findOne({
+            where: ({id: productId}), 
+            include: [
+                {association: "orders"}
+            ]
+        });
+
+        res.json({data: oneProduct}); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const createProduct = async(req, res) => {
+    
+    const productData = {
+        product_name: req.body.product_name,
+        product_img: req.body.product_img,
+        product_description: req.body.product_description,
+        product_price: req.body.product_price
+    }
+    
+    try {
+
+        const product = await Product.create(productData);
+
+        res.statut(201).json(product); 
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error});
+    }
+}
+
+const updateProduct = async(req, res) => {
+    try {
+
+        const updatedProduct = await Product.findOne({
+            where: {id: req.params.id}
+        }); 
+
+        if(!updatedProduct) {
+            return res.status(404).json({error: "aucun produit"}); 
+        }
+
+        const {
+            product_name, 
+            product_img, 
+            product_description,
+            product_price
+        } = req.body
+
+        if(product_name) {
+            updatedProduct.product_name = product_name; 
+        }
+        if(product_img) {
+            updatedProduct.product_img = product_img; 
+        }
+        if(product_description) {
+            updatedProduct.product_description = product_description; 
+        }
+        if(product_price) {
+            updatedProduct.product_price = product_price; 
+        }
+    
+        await updatedProduct.save();
+
+        res.json({data: updatedProduct});
+        
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({error});
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const deletedProduct = await Product.findOne({
+            where: {id: request.params.id}
+        }); 
+
+
+        if(!deletedProduct) {
+            return res.status(404).json({error: "Aucun produit"});
+        }
+
+        await deletedProduct.destroy();
+
+        res.json({data: deletedProduct});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+}
+
+export default {
+    getAllProducts,
+    getOneProduct, 
+    createProduct, 
+    updateProduct,
+    deleteProduct
+}
