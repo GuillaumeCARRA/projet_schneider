236d0bd4be63a801b967f236e8e9a3d4cb1b3166
GuillaumeCARRA <carraguillaume@gmail.com>
2023-05-24 10:49:06 +0200

    setting up controller

--- Back/app/controller/faqAnswerController.js
+++ Back/app/controller/faqAnswerController.js
@@ -1,1 +1,113 @@
 import FaqAnswer from "../models/faqAnswer.js";
+
+
+const getAllFaqAnswers = async (req, res) => {
+    try {
+        const faqAnswers = await FaqAnswer.findAll({
+            include: [
+                {association: "asks"}
+            ]
+        });
+
+        res.json({data: faqAnswers}); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const getOneFaqAnswer = async (req, res) => {
+    try {
+        const faqAnswerId = req.params.id; 
+
+        const oneAnswer = await FaqAnswer.findOne({
+            where: ({id: faqAnswerId}), 
+            include: [
+                {association: "asks"}
+            ]
+        });
+
+        res.json({data: oneAnswer}); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const createFaqAnswer = async(req, res) => {
+    
+    const faqAnswerData = {
+        answer: req.body.answer
+    }
+    
+    try {
+
+        const faqAnswer = await FaqAnswer.create(faqAnswerData);
+
+        res.statut(201).json(faqAnswer); 
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error});
+    }
+}
+
+const updateFaqAnswer = async(req, res) => {
+    try {
+
+        const updatedFaqAnswer = await FaqAnswer.findOne({
+            where: {id: req.params.id}
+        }); 
+
+        if(!updatedFaqAnswer) {
+            return res.status(404).json({error: "aucunes réponses"}); 
+        }
+
+        const {
+            answer
+        } = req.body
+
+        if(answer) {
+            updatedFaqAnswer.answer = answer; 
+        }
+
+        await updatedFaqAnswer.save();
+
+        res.json({data: updatedFaqAnswer});
+        
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({error});
+    }
+}
+
+const deleteFaqAnswer = async (req, res) => {
+    try {
+        const deletedFaqAnswer = await FaqAnswer.findOne({
+            where: {id: request.params.id}
+        }); 
+
+
+        if(!deletedFaqAnswer) {
+            return res.status(404).json({error: "Aucune réponse"});
+        }
+
+        await deletedFaqAnswer.destroy();
+
+        res.json({data: deletedFaqAnswer});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+}
+
+export default {
+    getAllFaqAnswers, 
+    getOneFaqAnswer, 
+    createFaqAnswer, 
+    updateFaqAnswer,
+    deleteFaqAnswer
+}
--- Back/app/controller/faqAskController.js
+++ Back/app/controller/faqAskController.js
@@ -1,1 +1,116 @@
 import FaqAsk from "../models/faqAsk.js"
+
+const getAllFaqAsks = async (req, res) => {
+    try {
+        const faqAsks = await FaqAsk.findAll({
+            include: [
+                {association: "faqCategories"},
+                {association: 'answers'}
+            ]
+        });
+
+        res.json({data: faqAsks}); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const getOneFaqAsk = async (req, res) => {
+    try {
+        const faqAskId = req.params.id; 
+
+        const oneAsk = await FaqAsk.findOne({
+            where: ({id: faqAskId}), 
+            include: [
+                {association: "faqCategories"},
+                {association: 'answers'}
+            ]
+        });
+
+        res.json({data: oneAsk}); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error}); 
+    }
+}
+
+const createFaqAsk = async(req, res) => {
+    
+    const faqAskData = {
+        ask: req.body.ask,
+        faq_answer_id: req.body.faq_answer_id,
+        faq_category_id: req.body.faq_category_id
+    }
+    
+    try {
+
+        const faqAsk = await FaqAsk.create(faqAskData);
+
+        res.statut(201).json(faqAsk); 
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error});
+    }
+}
+
+const updateFaqAsk = async(req, res) => {
+    try {
+
+        const updatedFaqAsk = await FaqAsk.findOne({
+            where: {id: req.params.id}
+        }); 
+
+        if(!updatedFaqAsk) {
+            return res.status(404).json({error: "aucunes questions"}); 
+        }
+
+        const {
+            ask
+        } = req.body
+
+        if(ask) {
+            updatedFaqAsk.ask = ask; 
+        }
+
+        await updatedFaqAsk.save();
+
+        res.json({data: updatedFaqAsk});
+        
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({error});
+    }
+}
+
+const deleteFaqAsk = async (req, res) => {
+    try {
+        const deletedFaqAsk = await FaqAsk.findOne({
+            where: {id: request.params.id}
+        }); 
+
+
+        if(!deletedFaqAsk) {
+            return res.status(404).json({error: "Aucune question"});
+        }
+
+        await deletedFaqAsk.destroy();
+
+        res.json({data: deletedFaqAsk});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+}
+
+export default {
+    getAllFaqAsks, 
+    getOneFaqAsk, 
+    createFaqAsk, 
+    updateFaqAsk,
+    deleteFaqAsk
+}
