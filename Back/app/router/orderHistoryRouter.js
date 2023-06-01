faafe809d87f7fb5d567fa2a7a9cdfb39fba57b7
GuillaumeCARRA <carraguillaume@gmail.com>
2023-06-01 13:53:23 +0200

    add function for orderHistoryController

--- Back/app/router/orderHistoryRouter.js
+++ Back/app/router/orderHistoryRouter.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.get('/', orderHistoryController.getAllOrderHistories);
 router.get('/:id(\\d+)', orderHistoryController.getOneOrderHistory);
-
-
+router.post('/', orderHistoryController.createOrderHistory);
+router.patch('/:id(\\d+)', orderHistoryController.updateOrderHistory);
+router.delete('/:id(\\d+)', orderHistoryController.deleteOrderHistory);
 
 export default router; 
